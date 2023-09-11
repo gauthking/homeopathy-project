80585791fda4cb55a43db463dac5c3458fb1e615
vipin <vipin.bhati2020@vitstudent.ac.in>
2023-09-11 16:02:41 +0530

    fixed className errros

--- src/components/page-container.tsx
+++ src/components/page-container.tsx
@@ -4,8 +4,14 @@ interface IPageContainerProps {
   children: React.ReactNode;
 }
 
+/**
+ *
+ * @param param0 ReactNode
+ * @returns JSX.Element
+ * @description function act as base container for website
+ */
 const PageContainer = ({ children }: IPageContainerProps) => {
-  return <div className="">{children}</div>;
+  return <div className="container">{children}</div>;
 };
 
 export default PageContainer;
