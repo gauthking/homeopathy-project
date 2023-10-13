4aaed6c3f5c156883becf6811f202ceb7a1dfcc1
Gautham Krishna <90638995+gauthking@users.noreply.github.com>
2023-10-13 15:07:18 +0530

    final changes

--- src/components/about-section.tsx
+++ src/components/about-section.tsx
@@ -8,7 +8,7 @@ const AboutSection = () => {
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth <= 640) {
-        setOffset(["0 1", "1.6 4.8"]); // Adjust the offset for mobile screens
+        setOffset(["0 1", "1.6 4.9"]); // Adjust the offset for mobile screens
       } else {
         setOffset(["0 1", "1.6 3.5"]);
       }
@@ -52,24 +52,24 @@ const AboutSection = () => {
           <h3 className="font-bold text-3xl">About us</h3>
           <h1 className="font-medium text-3xl">We help Your Health</h1>
           <p className=" text-lg text-justify  text-neutral-800 max-w-[600px]">
-            Joy Homeo Clinic in Perambur, Chennai is a top player in the
-            category Homeopathic Clinics in Chennai. This well-known
-            establishment acts as an one-stop destination servicing customers
-            both local and from other parts of Chennai. Over the course of its
-            journey, this business has established a firm foothold in it’s
-            industry. The belief that customer satisfaction is as important as
-            their products and services, have helped this establishment garner a
-            vast base of customers, which continues to grow by the day. This
-            business employs individuals that are dedicated towards their
-            respective roles and put in a lot of effort to achieve the common
-            vision and larger goals of the company. In the near future, this
-            business aims to expand its line of products and services and cater
-            to a larger client base. In Chennai, this establishment occupies a
-            prominent location in Perambur. It is an effortless task in
-            commuting to this establishment as there are various modes of
-            transport readily available. It is at Bunder Garden 2nd Street ,
-            Near Subiksha Nursing Home, which makes it easy for first-time
-            visitors in locating this establishment.
+            Joy Homeo Clinic in Perambur is a top player in the category
+            Homeopathic Clinics in Chennai. This well-known establishment acts
+            as an one-stop destination servicing customers both local and from
+            other parts of Chennai. Over the course of its journey, this
+            business has established a firm foothold in it’s industry. The
+            belief that customer satisfaction is as important as their products
+            and services, have helped this establishment garner a vast base of
+            customers, which continues to grow by the day. This business employs
+            individuals that are dedicated towards their respective roles and
+            put in a lot of effort to achieve the common vision and larger goals
+            of the company. In the near future, this business aims to expand its
+            line of products and services and cater to a larger client base. In
+            Chennai, this establishment occupies a prominent location in
+            Perambur. It is an effortless task in commuting to this
+            establishment as there are various modes of transport readily
+            available. It is at Bunder Garden 2nd Street , Near Subiksha Nursing
+            Home, which makes it easy for first-time visitors in locating this
+            establishment.
           </p>
           {/* <CustomButton className="max-w-fit mt-4 bg-primary text-white font-medium rounded-md cursor-pointer hover:bg-primary/80 px-6 py-2 text-sm">
           More About us
